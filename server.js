3de337f62b68e2f902618107db05ee629f0d259a
mrayhanhakim <mrayhanhakim2@gmail.com>
2020-03-04 08:18:38 +0700

    Move game dir to dist

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+var history = require('connect-history-api-fallback');
+
+const app = express();
+
+app.use(history({
+  verbose: true
+}));
+
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port);
